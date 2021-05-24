19cc512b68327f525aaeda2cf3ebd71cb93c9b49
Mona Abu Sharkh <mona@Monas-MacBook-Air.local>
2021-05-24 17:23:15 +0300

    solved

---
+++ story.js
@@ -0,0 +1,8 @@
+let name = "Mona";
+let Age = 17;
+let Hobby = "baking";
+console.log(
+  `Ms. ${name} in 33 years, you will be ${
+    Age + 33
+  } years old. By then you will have decades of ${Hobby} practice`
+);
